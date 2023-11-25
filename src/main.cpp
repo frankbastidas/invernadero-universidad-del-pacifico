9529ff9ab895f87c56e347b3910e1f34202d1aad
FranklinBastidas <fgbcuya@inf.ufrgs.br>
2023-11-25 01:19:11 -0500

    menus actualizados, ctrlTemp,ctrlLuz,grabardatos

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,11 @@ unsigned int temp_ctrl = 0;
 unsigned int hum_ctrl = 0;
 bool temp_estado_ctrl = false;
 char temp_estado_ctrl_text[4]; // {'O', 'F', 'F', '\0'}
+char luz_estado_ctrl_text[4]; // {'O', 'F', 'F', '\0'}
 char string_on[] = "Act";
 char string_off[] = "Des";
+bool luz_estado_ctrl = false;
+
 
 // ------ Configuración de Menus/submenus
 //Menus
@@ -28,6 +31,8 @@ LiquidScreen pantallaCtrlTemperatura;
 LiquidScreen pantallaCtrlTemperatura_ctrlStatus;
 LiquidScreen pantallaCtrlTemperatura_ctrlTemp;
 LiquidScreen pantallaCtrlLuz;
+LiquidScreen pantallaCtrlLuz_ctrlStatus;
+LiquidScreen pantallaCtrlLuz_ctrlLuz;
 LiquidScreen pantallaGrabar;
 
 //menuPrincipal
@@ -38,9 +43,9 @@ LiquidLine Prin_L4(1, 1, "Grabar Datos");
 
 //menuMonitorizar
 LiquidLine Mon_L1_1(0, 0, "T:");
-LiquidLine Mon_L1_2(2, 0, temp_sen, "/", temp_ctrl, "\337");
+LiquidLine Mon_L1_2(2, 0, temp_sen, "\337", temp_ctrl, "\337");
 LiquidLine Mon_L1_3(8, 0, "H:");
-LiquidLine Mon_L1_4(10, 0, hum_sen, "/", hum_ctrl, "%");
+LiquidLine Mon_L1_4(10, 0, hum_sen, "%", hum_ctrl, "%");
 LiquidLine Mon_L2_1(0, 1, "CtrlTemp:  ", temp_estado_ctrl_text);
 
 //menuCtrlTemperatura
@@ -49,12 +54,32 @@ LiquidLine CtrlTemp_L2(1, 1, "Ctrl Temp: ", temp_estado_ctrl_text);
 LiquidLine CtrlTemp_L3(1, 1, "Temperatura:", temp_ctrl, "\337");
 
 //menuCtrlTemperatura_ctrlStatus
-LiquidLine CtrlTemp_2_L1(1, 0, "Ctrl Temp:");
-LiquidLine CtrlTemp_2_L2(1, 1, temp_estado_ctrl_text);
+// LiquidLine CtrlTemp_2_L1(1, 0, "Ctrl Temp:");
+// LiquidLine CtrlTemp_2_L2(1, 1, temp_estado_ctrl_text);
 
 //menuCtrlTemperatura_ctrlTemp
-LiquidLine CtrlTemp_3_L1(1, 0, "Temperatura:");
-LiquidLine CtrlTemp_3_L2(1, 1, temp_ctrl);
+// LiquidLine CtrlTemp_3_L1(1, 0, "Temperatura:");
+// LiquidLine CtrlTemp_3_L2(1, 1, temp_ctrl);
+
+//menuCtrlLuz
+LiquidLine CtrlLuz_L1(1, 0, "Menu Principal");
+LiquidLine CtrlLuz_L2(1, 1, "Ctrl Luz: ", luz_estado_ctrl_text);
+LiquidLine CtrlLuz_L3(1, 1, "Luz:", luz_estado_ctrl, "\337");
+
+// //menuCtrlTemperatura_ctrlStatus
+// LiquidLine CtrlLuz_2_L1(1, 0, "Ctrl Luz:");
+// LiquidLine CtrlLuz_2_L2(1, 1, luz_estado_ctrl_text);
+
+// //menuCtrlTemperatura_ctrlTemp
+// LiquidLine CtrlLuz_3_L1(1, 0, "Luz:");
+// LiquidLine CtrlLuz_3_L2(1, 1, luz_estado_ctrl);
+
+//menuGrabar
+LiquidLine Grabar_L1(1, 0, "Menu Principal");
+// LiquidLine Grabar_L2(1, 1, "Estado Rec:", luz_estado_ctrl_text);
+LiquidLine Grabar_L3(1, 1, "Temp Rec: ", luz_estado_ctrl, "\337");
+// LiquidLine Grabar_L4(1, 1, "Hum Rec: ", luz_estado_ctrl, "%");
+
 
 LiquidMenu menuInvernadero (lcd, pantallaMenuPrincipal);
 
@@ -74,6 +99,9 @@ static DS1307 RTC;
 // ------ Configuración SD
 const int chipSelect = 4;
 
+// ------ Configuración btn1test
+#define bt1 10
+#define bt2 9
 
 
 // the follow variables is a long because the time, measured in miliseconds
@@ -130,6 +158,30 @@ void fn_ctrlTemp()
   delay(100);
 }
 
+void fn_ctrlLuz()
+{
+  fcline_menuAnterior = menuInvernadero.get_focusedLine();
+  if (luz_estado_ctrl)
+    strncpy(luz_estado_ctrl_text, string_on, sizeof(string_on));
+  else
+    strncpy(luz_estado_ctrl_text, string_off, sizeof(string_off));
+  menuInvernadero.change_screen(4);
+  menuInvernadero.set_focusedLine(0);
+  delay(100);
+}
+
+void fn_grabar()
+{
+  fcline_menuAnterior = menuInvernadero.get_focusedLine();
+  if (luz_estado_ctrl)
+    strncpy(luz_estado_ctrl_text, string_on, sizeof(string_on));
+  else
+    strncpy(luz_estado_ctrl_text, string_off, sizeof(string_off));
+  menuInvernadero.change_screen(5);
+  menuInvernadero.set_focusedLine(0);
+  delay(100);
+}
+
 void fn_principal()
 {
   menuInvernadero.change_screen(1);
@@ -145,6 +197,7 @@ void selectOption(){
     else
       pantalla_anterior();
     menuInvernadero.update();
+    while (digitalRead(encoderBotonPin)==LOW);
     delay(100);
   }
 }
@@ -153,12 +206,14 @@ void setup() {
 
   // ------ Iniciar Entradas/Salidas
   pinMode(encoderBotonPin, INPUT);
+  pinMode(bt1, INPUT);
+  pinMode(bt2, INPUT);
 
   // ------ Iniciar LCD
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.println("Universidad del");
+  lcd.println(" Universidad del");
   lcd.setCursor(0, 1);
   lcd.println("    Pacifico");
   delay(500);
@@ -217,8 +272,8 @@ void setup() {
 
   Prin_L1.attach_function(1, fn_monitorizar);
   Prin_L2.attach_function(1, fn_ctrlTemp);
-  Prin_L3.attach_function(1, blankFunction);
-  Prin_L4.attach_function(1, blankFunction);
+  Prin_L3.attach_function(1, fn_ctrlLuz);
+  Prin_L4.attach_function(1, fn_grabar);
 
   pantallaMenuPrincipal.set_focusPosition(Position::LEFT);
   pantallaMenuPrincipal.set_displayLineCount(2);
@@ -245,15 +300,49 @@ void setup() {
   menuInvernadero.add_screen(pantallaCtrlTemperatura); // pantalla 3
 
   //menuCtrlTemperatura_ctrlStatus
-  pantallaCtrlTemperatura_ctrlTemp.add_line(CtrlTemp_L1);
-  pantallaCtrlTemperatura_ctrlTemp.add_line(CtrlTemp_L2);
+  // pantallaCtrlTemperatura_ctrlTemp.add_line(CtrlTemp_2_L1);
+  // pantallaCtrlTemperatura_ctrlTemp.add_line(CtrlTemp_2_L2);
 
-  CtrlTemp_L2.attach_function(1, blankFunction);
-  CtrlTemp_L2.attach_function(2, blankFunction);
+  // CtrlTemp_2_L1.attach_function(1, blankFunction);
+  // CtrlTemp_2_L2.attach_function(2, blankFunction);
+
+  //menuCtrlLuz
+  pantallaCtrlLuz.add_line(CtrlLuz_L1);
+  pantallaCtrlLuz.add_line(CtrlLuz_L2);
+  pantallaCtrlLuz.add_line(CtrlLuz_L3);
+
+  CtrlLuz_L1.attach_function(1, fn_principal);
+  CtrlLuz_L2.attach_function(1, blankFunction);
+  CtrlLuz_L3.attach_function(1, blankFunction);
+
+  pantallaCtrlLuz.set_focusPosition(Position::LEFT);
+  pantallaCtrlLuz.set_displayLineCount(2);
+  menuInvernadero.add_screen(pantallaCtrlLuz); // pantalla 4
+
+  //menuCtrlLuz_ctrlStatus
+  // pantallaCtrlLuz_ctrlLuz.add_line(CtrlTemp_L1);
+  // pantallaCtrlLuz_ctrlLuz.add_line(CtrlTemp_L2);
+
+  // CtrlTemp_L2.attach_function(1, blankFunction);
+  // CtrlTemp_L2.attach_function(2, blankFunction);
 
   // pantallaCtrlTemperatura_ctrlTemp.set_focusPosition(Position::LEFT);
   // pantallaCtrlTemperatura_ctrlTemp.set_displayLineCount(2);
-  menuInvernadero.add_screen(pantallaCtrlTemperatura_ctrlTemp); // pantalla 3
+  // menuInvernadero.add_screen(pantallaCtrlTemperatura_ctrlTemp); // pantalla 3
+
+  //menuGrabar
+  pantallaGrabar.add_line(Grabar_L1);
+  // pantallaGrabar.add_line(Grabar_L2);
+  pantallaGrabar.add_line(Grabar_L3);
+  // pantallaGrabar.add_line(Grabar_L4);
+
+  Grabar_L1.attach_function(1, fn_principal);
+  // Grabar_L2.attach_function(1, blankFunction);
+  Grabar_L3.attach_function(1, blankFunction);
+
+  pantallaGrabar.set_focusPosition(Position::LEFT);
+  pantallaGrabar.set_displayLineCount(2);
+  menuInvernadero.add_screen(pantallaGrabar); // pantalla 5
 
   //sistema de menus
   menuInvernadero.init();
@@ -286,7 +375,22 @@ if (menuInvernadero.is_callable(1)){
     oldPosition = newPosition;
     delay(300);
   }
+  if(digitalRead(bt1)==LOW){
+    menuInvernadero.switch_focus(true);
+    menuInvernadero.update();
+    while (digitalRead(bt1)==LOW);
+    delay(100);
+  }else if (digitalRead(bt2)==LOW)
+  {
+    menuInvernadero.switch_focus(false);
+    menuInvernadero.update();
+    while (digitalRead(bt2)==LOW);
+    delay(100);
+  }
+  
 }
+
+
 // if(menuInvernadero.)
   //lcd.setCursor(1, 1);
   //lcd.print(SHT2x_LIB_VERSION);
